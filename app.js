5fb43a1d9d3f1aba6402f6c55f9897c4d2e03836
Glitch (coral-atrociraptor) <none>
2019-12-10 04:01:59 +0000

    🐀🎖 Add a modal with input to add to-do items

--- app.js
+++ app.js
@@ -8,34 +8,33 @@ const app = new App({
 
 const test = [
   {
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "To Do Item"
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: "To Do Item"
     }
   }
 ];
 
 const button = {
-  "type": "actions",
-  "elements": [
+  type: "actions",
+  elements: [
     {
-      "type": "button",
-      "text": {
-        "type": "plain_text",
-        "text": "Click me!"
+      type: "button",
+      text: {
+        type: "plain_text",
+        text: "Click me!"
       },
-     "action_id": "button_abc",
+      action_id: "button_abc"
     }
   ]
 };
 
 // All the room in the world for your code
-app.event('app_home_opened', async ({ event, context }) => {
+app.event("app_home_opened", async ({ event, context }) => {
   try {
     /* view.publish is the method that your app uses to push a view to the Home tab */
-    const result = await app.client.views.publish({
-
+    await app.client.views.publish({
       /* retrieves your xoxb token from context */
       token: context.botToken,
 
@@ -44,69 +43,101 @@ app.event('app_home_opened', async ({ event, context }) => {
 
       /* the view payload that appears in the app home*/
       view: {
-        type: 'home',
-        callback_id: 'home_view',
+        type: "home",
+        callback_id: "home_view",
 
         /* body of the view */
-        blocks: [
-          ...test,
-          button,
-        ]
+        blocks: [...test, button]
       }
     });
-  }
-  catch (error) {
+  } catch (error) {
     console.error(error);
   }
 });
-
 
 // Listen for a button invocation with action_id `button_abc`
 // You must set up a Request URL under Interactive Components on your app configuration page
-app.action('button_abc', async ({ ack, body, context }) => {
+app.action("button_abc", async ({ ack, body, context }) => {
   // Acknowledge the button request
   ack();
-  test.push({
-    "type": "section",
-    "text": {
-      "type": "mrkdwn",
-      "text": "To Do Item"
-    }
-  })
   try {
-    console.log('try', body);
-    // Update the message
-     await app.client.views.publish({
-
+    await app.client.views.open({
       /* retrieves your xoxb token from context */
       token: context.botToken,
-
-      /* the user that opened your app's app home */
       user_id: body.user.id,
-
-      /* the view payload that appears in the app home*/
+      trigger_id: body.trigger_id,
+      response_action: "push",
       view: {
-        type: 'home',
-        callback_id: 'home_view',
-
-        /* body of the view */
+        type: "modal",
+        callback_id: "add_modal",
+        title: {
+          type: "plain_text",
+          text: "Add a To Do Item"
+        },
+        submit: {
+          type: "plain_text",
+          text: "Submit"
+        },
+        close: {
+          type: "plain_text",
+          text: "Cancel"
+        },
         blocks: [
-          ...test,
-          button,
-        ], 
+          {
+            block_id: "to_do",
+            type: "input",
+            element: {
+              type: "plain_text_input",
+              action_id: "input",
+              placeholder: {
+                type: "plain_text",
+                text: "give yourself a pat on the back"
+              }
+            },
+            label: {
+              type: "plain_text",
+              text: "To do"
+            }
+          }
+        ]
       }
     });
-    console.log('here');
-  }
-  catch (error) {
-    console.error(error);
+  } catch (error) {
+    test.push();
+    console.error(error.data.response_metadata.messages);
   }
 });
 
+// All the room in the world for your code
+app.view("add_modal", async ({ ack, body, view, context }) => {
+  ack();
+  test.push({
+    type: "section",
+    text: {
+      type: "mrkdwn",
+      text: view.state.values.to_do.input.value
+    }
+  });
+  try {
+    console.log("submitted", body);
+    await app.client.views.publish({
+      token: context.botToken,
+      user_id: body.user.id,
+
+      view: {
+        type: "home",
+        callback_id: "home_view",
+        blocks: [...test, button]
+      }
+    });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 (async () => {
   // Start your app
   await app.start(process.env.PORT || 3000);
 
-  console.log('⚡️ Bolt app is running!');
+  console.log("⚡️ Bolt app is running!");
 })();
